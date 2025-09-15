6f119a693eb47dbc3444a6240f8890d5a5c69789
Aakanksha Chaudhari <aakankshac773@gmail.com>
2025-09-15 23:39:53 +0530

    Add backend code

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,10 +1,14 @@
-const mysql = require("mysql2");
+import mysql from "mysql2";
+import dotenv from "dotenv";
+
+dotenv.config();
 
 const db = mysql.createConnection({
-  host: "localhost",
-  user: "root",
-  password: "aakanksha1212@",
-  database: "store_rating",
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
+  port: process.env.DB_PORT
 });
 
 db.connect((err) => {
@@ -15,4 +19,4 @@ db.connect((err) => {
   }
 });
 
-module.exports = db;
+export default db;
